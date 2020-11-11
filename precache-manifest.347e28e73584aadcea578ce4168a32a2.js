4ea5ae312d065c0f93754696a7d318089b12cd70
Gabriel Q. Matos <gabriel.matos@fpf.br>
2020-11-11 17:36:56 -0400

    Updates

---
+++ precache-manifest.347e28e73584aadcea578ce4168a32a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8550d5530f9ea5cf1bc58132bc0b860",
+    "url": "/uboot-reactjs2/index.html"
+  },
+  {
+    "revision": "ea1bf26c8fe1f0bca74c",
+    "url": "/uboot-reactjs2/static/css/main.18ed8c03.chunk.css"
+  },
+  {
+    "revision": "124ad3312759af171e69",
+    "url": "/uboot-reactjs2/static/js/2.518af857.chunk.js"
+  },
+  {
+    "revision": "70a3d7601f1f6b9195446c76f063f9cc",
+    "url": "/uboot-reactjs2/static/js/2.518af857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea1bf26c8fe1f0bca74c",
+    "url": "/uboot-reactjs2/static/js/main.e95d397f.chunk.js"
+  },
+  {
+    "revision": "045c606a756f03a4454e",
+    "url": "/uboot-reactjs2/static/js/runtime-main.a58c2c0e.js"
+  }
+]);
